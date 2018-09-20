9d617a3cbbd4b8c4a5933f433667265d32eb0ad2
fer96 <fer96.dlrs@gmail.com>
2018-09-19 22:57:39 -0500

    done until part 5

---
+++ entregables/Restaurant/Restaurant/MenuController.swift
@@ -0,0 +1,72 @@
+//
+//  MenuController.swift
+//  Restaurant
+//
+//  Created by Fernando De La Rosa on 19/09/18.
+//  Copyright © 2018 DeLaRosa Fernando. All rights reserved.
+//
+
+import UIKit
+
+class MenuController {
+    let basedURL = URL(string: "http://localhost:8090/")!
+    
+    func fetchCategories(completion: @escaping ([String]?) -> Void){
+        let categoryURL = basedURL.appendingPathComponent("categories")
+        let task = URLSession.shared.dataTask(with: categoryURL){ (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            
+            if let data = data,
+                let categories = try? jsonDecoder.decode(Categories.self, from: data) {
+                completion(categories.categories)
+            } else {
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+    
+    func fetchMenuItems(categoryName: String, completion: @escaping ([MenuItem]?) -> Void) {
+        let initialMenuURL =  basedURL.appendingPathComponent("menu")
+        var components = URLComponents(url: initialMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        let menuURL = components.url!
+        let task = URLSession.shared.dataTask(with: menuURL){ (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            
+            if let data = data,
+                let menuItems = try? jsonDecoder.decode(MenuItems.self, from: data) {
+                completion(menuItems.items)
+            } else {
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+    
+    func submitOrder(menuIds: [Int], completion: @escaping (Int?) -> Void) {
+        let orderURL = basedURL.appendingPathComponent("order")
+        
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let data = ["menuIds": menuIds]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: orderURL){ (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            
+            if let data = data,
+                let preparationTime = try? jsonDecoder.decode(PreparationTime.self, from: data) {
+                completion(preparationTime.prepTime)
+            } else {
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+    
+    
+}
---
+++ entregables/Restaurant/Restaurant/MenuTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MenuTableViewController.swift
+//  Restaurant
+//
+//  Created by Fernando De La Rosa on 19/09/18.
+//  Copyright © 2018 DeLaRosa Fernando. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+
+    var category: String!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
