587bbb23b26783cef4ad503e6afedad9488758e6
fer96 <fer96.dlrs@gmail.com>
2018-11-16 19:32:00 -0600

    Create a new project.  Collection View

---
+++ actividades/FlickerSearch/FlickerSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickerSearch
+//
+//  Created by DelaRosa Fernando on 11/16/18.
+//  Copyright © 2018 DeLaRosa Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
