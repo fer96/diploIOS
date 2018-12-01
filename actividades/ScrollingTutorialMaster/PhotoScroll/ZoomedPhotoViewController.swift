470c69781b74d66b9f9be4e4f6046b6b18540a31
fer96 <fer96.dlrs@gmail.com>
2018-11-30 20:48:04 -0600

    Finish ScrollView

--- actividades/ScrollingTutorialMaster/PhotoScroll/ZoomedPhotoViewController.swift
+++ actividades/ScrollingTutorialMaster/PhotoScroll/ZoomedPhotoViewController.swift
@@ -32,6 +32,11 @@ import UIKit
 
 class ZoomedPhotoViewController: UIViewController {
   @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+  @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+  @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+  @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
   var photoName: String?
   
   override func viewDidLoad() {
@@ -40,6 +45,39 @@ class ZoomedPhotoViewController: UIViewController {
     }
   }
   
+  func updateMinZoomScaleForSize(_ size: CGSize){
+    let widthScale = size.width / imageView.bounds.width
+    let heightScale = size.height / imageView.bounds.height
+    let minScale = min(widthScale,heightScale)
+    
+    scrollView.minimumZoomScale = minScale
+    scrollView.zoomScale = minScale
+  }
+  
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    updateMinZoomScaleForSize(view.bounds.size)
+  }
 }
 
+
+extension ZoomedPhotoViewController: UIScrollViewDelegate{
+  func viewForZooming( in scrollView: UIScrollView) -> UIView?{
+    return imageView
+  }
+  func updateConstraintsForSize(_ size: CGSize){
+    let yOffSet = max(0, (size.height - imageView.frame.height) / 2)
+    imageViewTopConstraint.constant = yOffSet
+    imageViewBottomConstraint.constant = yOffSet
+    
+    let xOffSet = max(0, (size.width - imageView.frame.width) / 2)
+    imageViewLeadingConstraint.constant =  xOffSet
+    imageViewTrailingConstraint.constant = xOffSet
+    
+    view.layoutIfNeeded()
+  }
+  func scrollViewDidZoom(_ scrollView: UIScrollView) {
+    updateConstraintsForSize(view.bounds.size)
+  }
+}
 
