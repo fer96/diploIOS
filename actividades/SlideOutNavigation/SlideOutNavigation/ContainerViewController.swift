6bd3b8954a3ce344d41e731d42ba5871032fb7b9
fer96 <fer96.dlrs@gmail.com>
2018-11-30 19:07:07 -0600

    Rename folder SlideOutNavigation

---
+++ actividades/SlideOutNavigation/SlideOutNavigation/ContainerViewController.swift
@@ -0,0 +1,160 @@
+/// Copyright (c) 2017 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+import QuartzCore
+
+class ContainerViewController: UIViewController {
+  
+  enum SlideOutState {
+    case bothCollapsed
+    case leftPanelExpanded
+    case rightPanelExpanded
+  }
+  
+  var centerNavigationController: UINavigationController!
+  var centerViewController: CenterViewController!
+  
+  var currentState: SlideOutState = .bothCollapsed
+  var letfViewController: SidePanelViewController?
+  var rightViewController: SidePanelViewController?
+  let centerPanelExpandedOffSet: CGFloat = 60
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    centerViewController = UIStoryboard.centerViewController()
+    centerViewController.delegate = self
+    centerNavigationController = UINavigationController(rootViewController: centerViewController)
+    view.addSubview(centerNavigationController.view)
+    addChildViewController(centerNavigationController)
+    centerNavigationController.didMove(toParentViewController: self)
+  }
+}
+
+extension ContainerViewController: CenterViewControllerDelegate{
+  //LEFT
+  func toggleLeftPanel() {
+    let notAlreadyExpanded = (currentState != .leftPanelExpanded)
+    if notAlreadyExpanded{
+      addLeftPanelViewController()
+    }
+    animateLeftPanel(shouldExpand: notAlreadyExpanded)
+  }
+  func addLeftPanelViewController() {
+    guard letfViewController == nil else {
+      return
+    }
+    if let vc = UIStoryboard.leftViewController(){
+      vc.animals = Animal.allCats()
+      addChildSidePanelController(vc)
+      letfViewController = vc
+    }
+  }
+  func animateLeftPanel(shouldExpand: Bool){
+    if shouldExpand{
+      currentState = .leftPanelExpanded
+      animateCenterPanelXPosition(
+        targetPosition: centerNavigationController.view.frame.width - centerPanelExpandedOffSet)
+    }else {
+      animateCenterPanelXPosition(targetPosition: 0){ finished in
+        self.currentState = .bothCollapsed
+        self.willMove(toParentViewController: nil)
+        self.letfViewController?.view.removeFromSuperview()
+        self.letfViewController?.removeFromParentViewController()
+        self.letfViewController = nil
+      }
+    }
+  }
+  
+  //RIGHT
+  func toggleRightPanel() {
+    let notAlredyExpanded = (currentState != .rightPanelExpanded)
+    if notAlredyExpanded{
+      addRightPanelViewController()
+    }
+    animateRightPanel(shouldExpand: notAlredyExpanded)
+  }
+  func addRightPanelViewController() {
+    guard rightViewController == nil else{ return }
+    if let vc = UIStoryboard.rightViewController(){
+      vc.animals = Animal.allDogs()
+      addChildSidePanelController(vc)
+      rightViewController = vc
+    }
+  }
+  func animateRightPanel(shouldExpand: Bool){
+    if shouldExpand{
+      currentState = .rightPanelExpanded
+      animateCenterPanelXPosition(
+        targetPosition: centerPanelExpandedOffSet - centerNavigationController.view.frame.width)
+    }else {
+      animateCenterPanelXPosition(targetPosition: 0){ finished in
+        self.currentState = .bothCollapsed
+        self.willMove(toParentViewController: nil)
+        self.rightViewController?.view.removeFromSuperview()
+        self.rightViewController?.removeFromParentViewController()
+        self.rightViewController = nil
+      }
+    }
+  }
+  
+  
+  func addChildSidePanelController(_ sidePanelController: SidePanelViewController){
+    view.insertSubview(sidePanelController.view, at: 0)
+    addChildViewController(sidePanelController)
+    sidePanelController.didMove(toParentViewController: self)
+  }
+  func animateCenterPanelXPosition(targetPosition: CGFloat, completion: ((Bool) -> Void)? = nil){
+    UIView.animate(withDuration: 0.5,
+                   delay: 0,
+                   usingSpringWithDamping: 0.8,
+                   initialSpringVelocity: 0,
+                   options: .curveEaseInOut, animations: {
+                    self.centerNavigationController.view.frame.origin.x = targetPosition
+    }, completion: completion)
+  }
+  
+}
+
+private extension UIStoryboard {
+  
+  static func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: Bundle.main) }
+  
+  static func leftViewController() -> SidePanelViewController? {
+    return mainStoryboard().instantiateViewController(withIdentifier: "LeftViewController") as? SidePanelViewController
+  }
+  
+  static func rightViewController() -> SidePanelViewController? {
+    return mainStoryboard().instantiateViewController(withIdentifier: "RightViewController") as? SidePanelViewController
+  }
+  
+  static func centerViewController() -> CenterViewController? {
+    return mainStoryboard().instantiateViewController(withIdentifier: "CenterViewController") as? CenterViewController
+  }
+}
